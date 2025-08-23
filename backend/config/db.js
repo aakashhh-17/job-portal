aeb1335803a5cb5acc5453f7cc83a6c59fa7877d
aakashhh-17 <aakashbhujbal1704@gmail.com>
2025-08-23 13:40:44 +0530

    fixes

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async()=>{
     mongoose.connection.on('connected',()=> console.log('Database Connected!'))
 
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(`${process.env.MONGO_URI}/job-portal`)
 }
 
 export default connectDB
--- backend/controllers/webhooks.js
+++ backend/controllers/webhooks.js
@@ -24,9 +24,8 @@ export const clerkWebhooks = async (req, res) => {
         const userData = {
           _id: data.id,
           email: data.email_addresses[0].email_address,
-          // name: data.first_name+ " "+data.last_name,
+          name: data.first_name+ " " + data.last_name,
           image: data.image_url,
-          name: (`${data.first_name || ''} ${data.last_name || ''}`).trim(),
           resume: ''
         }
 
